d8b2b950a0d654839810993ad2a52c201b8b60e8
CrossBones1 <youssefhajjaj023@gmail.com>
2023-02-23 15:40:26 -0800

    c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,36 +2,18 @@
 #include <stdio.h>
 /**
  * main - Prints out the larget prime factor of 612852475143
- * Return: Always 0 with success
+ * Return:return 0 with success.
  */
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
+	long i;
+	long num = 612852475143;
 
-	n = 612852475143;
-	max = -1;
-
-	while (n % 2 == 0)
+	for (i = 2; i < num; i++)
 	{
-		max = 2;
-		n /= 2;
+		if (num % i == 0)
+			num /= i;
 	}
-
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
-	}
-
-	if (n > 2)
-		max = n;
-
-	printf("%ld\n", max);
-
+	printf("%li\n", num);
 	return (0);
 }
