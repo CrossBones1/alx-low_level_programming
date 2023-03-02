01cdfeff22d903dc023277a9f2b29e624cbb8a7c
CrossBones1 <youssefhajjaj023@gmail.com>
2023-03-02 14:00:03 -0800

    c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+
+char *leet(char *s)
+{
+	int stringCount, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+
+/*  scan through string */
+	stringCount = 0;
+	while (s[stringCount] != '\0')
+/* check whether leetLetter is found */
+	{
+		leetCount = 0;
+		while (leetCount < 10)
+		{
+			if (leetLetters[leetCount] == s[stringCount])
+			{
+				s[stringCount] = leetNums[leetCount];
+			}
+			leetCount++;
+		}
+		stringCount++;
+	}
+	return (s);
+}
