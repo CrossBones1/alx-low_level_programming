5902871d76cefb8e4ace0e0d2898d45328dfbd9b
CrossBones1 <youssefhajjaj023@gmail.com>
2023-03-02 14:02:08 -0800

    c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
 /**
- * leet - encode into 1337speak
- * @n: input value
- * Return: n value
+ *leet - encodes a string into 1337.
+ *letters a and A are replaced by 4.
+ *Letters e and E are replaced by 3
+ *Letters o and O are replaced by 0
+ *Letters t and T are replaced by 7
+ *Letters l and L are replaced by 1
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
  */
-
 char *leet(char *s)
 {
 	int stringCount, leetCount;
